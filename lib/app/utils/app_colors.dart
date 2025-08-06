449a5aa0fd12b164d1d183486cf2e9209e996208
rayhantmt <rayhantmt@gmail.com>
2025-08-07 01:06:33 +0600

    added the appcolors class

---
+++ lib/app/utils/app_colors.dart
@@ -0,0 +1,4 @@
+ import 'package:flutter/widgets.dart';
+class AppColors {
+ static const Color primarycolor = Color(0xff126535);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
        
       ),
       home: const Scaffold(
-        backgroundColor: Colors.white,
+        backgroundColor: AppColors.primarycolor
       ),
     );
   }
