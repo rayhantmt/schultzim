dc1ad3941c26b0034e6cbb2f08b366debcf665f9
rayhantmt <rayhantmt@gmail.com>
2025-08-09 03:55:35 +0600

    added the passeord field with obsecuring functionality

--- lib/app/modules/log_in/log_in_view.dart
+++ lib/app/modules/log_in/log_in_view.dart
@@ -2,92 +2,117 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:schultzim/app/common_widgets/common_button.dart';
 import 'package:schultzim/app/common_widgets/log_in_text_field.dart';
+
+import 'package:schultzim/app/modules/log_in/log_in_controller.dart';
 import 'package:schultzim/app/utils/app_colors.dart';
 
 import 'package:schultzim/app/utils/app_images.dart';
 
 class LogInView extends StatelessWidget {
-  const LogInView({super.key});
+  LogInView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final controrller = Get.find<LogInController>();
     return Scaffold(
       backgroundColor: Color(0xffDBE8E1),
       body: Padding(
-        padding: const EdgeInsets.only(left: 20 ,right: 20),
+        padding: const EdgeInsets.only(left: 20, right: 20),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            SizedBox(height: Get.height*0.1,),
-           Image.asset(AppImages.loginlogo,
-           height: 70,
-           width: 150,
-          // alignment: Alignment.center,
-           ),
-           Container(
-            height: Get.height*0.8,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
+            SizedBox(height: Get.height * 0.1),
+            Image.asset(
+              AppImages.loginlogo,
+              height: 70,
+              width: 150,
+              // alignment: Alignment.center,
             ),
-            child: Column(
-              children: [
-                Text('Log in',
-                style: TextStyle(
-                  fontWeight: FontWeight.w700,
-                  fontSize: 26,
-                  color: AppColors.primarycolor
-                ),
-                ),
-                SizedBox(height: 10,),
-                Text('Create an account or log in to explore our app',
-                style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontSize: 12,
-                  color: Color(0xff6C7278)
-                ),
-                ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Row(
-                    children: [
-                      Text('Email',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12,
-                        color: Color(0xff6C7278)
-                      ),
-                      )
-                    ],
+            Container(
+              height: Get.height * 0.8,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.white,
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    'Log in',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 26,
+                      color: AppColors.primarycolor,
+                    ),
                   ),
-                ),
-                SizedBox(height: 5,),
-                Formfield(tittle: 'Rayhantmt@gmail.com', obsecuretext: false, keyboardtype: TextInputType.emailAddress),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Row(
-                    children: [
-                      Text('Email',
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 12,
-                        color: Color(0xff6C7278)
-                      ),
-                      )
-                    ],
+                  SizedBox(height: 10),
+                  Text(
+                    'Create an account or log in to explore our app',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      fontSize: 12,
+                      color: Color(0xff6C7278),
+                    ),
                   ),
-                ),
-                SizedBox(height: 5,),
-                Formfield(tittle: 'Password', obsecuretext: false, keyboardtype: TextInputType.text),
-                CommonButton(tittle: 'Log In',),
-              ],
-            )),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                            color: Color(0xff6C7278),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Formfield(
+                    tittle: 'Rayhantmt@gmail.com',
+                    obsecuretext: false,
+                    keyboardtype: TextInputType.emailAddress,
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Row(
+                      children: [
+                        Text(
+                          'Email',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 12,
+                            color: Color(0xff6C7278),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Obx(
+                    () => Formfield(
+                      tittle: 'Password',
+                      obsecuretext: controrller.isObscured.value,
+                      keyboardtype: TextInputType.text,
+                      icon: GestureDetector(
+                        onTap: () => controrller.toggleObscureText(),
+                        child: Icon(controrller.isObscured.value
+                          ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        )),
+                    ),
+                  ),
+                  CommonButton(tittle: 'Log In'),
+                ],
+              ),
+            ),
           ],
         ),
-      )
+      ),
     );
   }
 }
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,11 +1,12 @@
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:schultzim/app/modules/log_in/log_in_binding.dart';
 import 'package:schultzim/app/modules/log_in/log_in_view.dart';
 import 'package:schultzim/app/modules/splash/splash_view.dart';
 import 'package:schultzim/app/routes/app_routes.dart';
 
 class AppPages {
   static final pages=[
-    GetPage(name: AppRoutes.login, page:() =>  LogInView()),
+    GetPage(name: AppRoutes.login, page:() =>  LogInView(),binding: LoginBinding()),
     GetPage(name: AppRoutes.splash, page: () => SplashView(),)
   ];
 }
