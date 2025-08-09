ba1482a22ec9d19cf1910334f46283b022624087
rayhantmt <rayhantmt@gmail.com>
2025-08-10 05:49:58 +0600

    done with the auth desing

--- lib/app/modules/change_password_success/change_password_success_view.dart
+++ lib/app/modules/change_password_success/change_password_success_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:schultzim/app/common_widgets/common_button.dart';
 
 class ChangePasswordSuccessView extends StatelessWidget {
   const ChangePasswordSuccessView({super.key});
@@ -7,16 +8,28 @@ class ChangePasswordSuccessView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20,right: 20),
         child: Column(
           children: [
+            SizedBox(height: 100,),
             Text('Forgot password',
             style: TextStyle(
               fontWeight: FontWeight.w700,
               fontSize: 26,
               color: Color(0xff090909)
             ),
-            )
+            ),
+            SizedBox(height: 30,),
+            Text('Your password has been successfully reset.',
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 16,
+              color: Color(0xff7D848D)
+            ),
+            ),
+            SizedBox(height: 30,),
+            CommonButton(tittle: 'Confirm')
           ],
         ),
       ),
