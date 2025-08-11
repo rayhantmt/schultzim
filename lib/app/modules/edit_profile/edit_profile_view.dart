c3011cbba12c22fc302f9f654d8d8883d61ed818
rayhantmt <rayhantmt@gmail.com>
2025-08-12 04:26:37 +0600

    added the basic design of eidt profile screen

--- lib/app/modules/edit_profile/edit_profile_view.dart
+++ lib/app/modules/edit_profile/edit_profile_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 class EditProfileView extends StatelessWidget {
   const EditProfileView({super.key});
@@ -6,7 +8,26 @@ class EditProfileView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amberAccent,
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height*0.25,
+            child: Row(
+              children: [],
+            ),
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
