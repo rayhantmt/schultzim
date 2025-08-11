963c96507f132dc13a21757899b09b66a9827fb5
rayhantmt <rayhantmt@gmail.com>
2025-08-12 04:19:22 +0600

    started edit profile screen

---
+++ lib/app/modules/edit_profile/edit_profile_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditProfileView extends StatelessWidget {
+  const EditProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amberAccent,
+    );
+  }
+}
