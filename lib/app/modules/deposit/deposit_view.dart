10b43c1e40ea7df91d46cee9ab162e4ac67689e9
rayhantmt <rayhantmt@gmail.com>
2025-08-13 04:07:24 +0600

    added the appbar of deposit

--- lib/app/modules/deposit/deposit_view.dart
+++ lib/app/modules/deposit/deposit_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 class DepositView extends StatelessWidget {
   const DepositView({super.key});
@@ -6,8 +8,50 @@ class DepositView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('deposit'),
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height*0.15,
+            child: Row(
+              children: [
+                GestureDetector(
+
+                  onTap: () => Get.back(),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Icon(Icons.arrow_circle_left_outlined,
+                    size: 35,
+                    color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox( width:  Get.width*0.25),
+                Text('Add Deposit',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.white
+                ),
+                )
+              ],
+            ),
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+            ),
+            child: Column(
+              children: [
+                
+              ],
+            ),
+          ))
+        ],
       ),
     );
   }
