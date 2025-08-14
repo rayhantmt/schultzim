e449f90cdc04b95e15c0005240fa2b30a1070f24
rayhantmt <rayhantmt@gmail.com>
2025-08-15 02:41:53 +0600

    fixed the error almost done with doposit success

--- lib/app/modules/withdraw/withdraw_binding.dart
+++ lib/app/modules/withdraw/withdraw_binding.dart
@@ -1,8 +1,9 @@
 import 'package:get/get.dart';
+import 'package:schultzim/app/modules/withdraw/withdraw_controller.dart';
 
 class WithdrawBinding extends Bindings{
   @override
   void dependencies() {
-   Get.lazyPut(<WithdrawController>() => WithdrawController,);
+   Get.lazyPut<WithdrawController>(() => WithdrawController());
   }
 }
