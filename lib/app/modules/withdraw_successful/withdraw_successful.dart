e6c03abb9b1b10418c7c2c99bc443374dcd1cd48
rayhantmt <rayhantmt@gmail.com>
2025-08-15 02:51:27 +0600

    done with desing of withdraw

--- lib/app/modules/withdraw_successful/withdraw_successful.dart
+++ lib/app/modules/withdraw_successful/withdraw_successful.dart
@@ -1,4 +1,12 @@
+
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/common_widgets/common_button.dart';
+
+import 'package:schultzim/app/utils/app_colors.dart';
+import 'package:schultzim/app/utils/app_images.dart';
 
 class WithdrawSuccessful extends StatelessWidget {
   const WithdrawSuccessful({super.key});
@@ -6,7 +14,62 @@ class WithdrawSuccessful extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height*0.25,
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Icon(Icons.arrow_back_ios_new_outlined,
+                  size: 35,
+                  color: Colors.white,
+                  ),
+                ),
+                SizedBox(width: Get.width*0.25,),
+                Text('Withdraw Successful',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  color: Colors.white
+                ),
+                )
+              ],
+            ),
+          ),
+          Expanded(child: Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+            ),
+            child: Column(
+              children: [
+                SizedBox(height: Get.height*0.15,),
+                SvgPicture.asset(AppImages.dpscful),
+                SizedBox(height: Get.height*0.05,),
+                Text('Withdraw successful!',
+                style: TextStyle(fontWeight: FontWeight.w600,
+                fontSize: 16,
+                color: AppColors.primarycolor
+                ),
+                ),
+                Spacer(),
+                CommonButton(tittle: 'Confirm',
+                
+                ),
+SizedBox(height: 40,)
+
+              ],
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
