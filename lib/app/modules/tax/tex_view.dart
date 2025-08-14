38e64450015a7afc0532a877b51d69b9fafad949
rayhantmt <rayhantmt@gmail.com>
2025-08-15 05:54:58 +0600

    almost done with tax screen

--- lib/app/modules/tax/tex_view.dart
+++ lib/app/modules/tax/tex_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 class TexView extends StatelessWidget {
   const TexView({super.key});
@@ -6,8 +8,75 @@ class TexView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Tax'),
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height * 0.25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Icon(
+                  Icons.arrow_circle_left_outlined,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Tax Information',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+              ),
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Text(
+                            '2023',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              fontSize: 32,
+                              color: Color(0xff050505),
+                            ),
+                          ),
+                          Container(
+                            height: 40,
+                            width: Get.width*0.35,
+                            decoration: BoxDecoration(
+                              color: Color(0xffEDF1F3),
+                              borderRadius: BorderRadius.circular(8),
+                              border: Border.all(width: 1),
+                            ),
+                            child: Center(child: Text('Change Year')),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
