215c21928f8a0134fae683e14a1441f08c1cf182
rayhantmt <rayhantmt@gmail.com>
2025-08-15 02:12:16 +0600

    working on withdraw screen

--- lib/app/modules/withdraw/withdraw_view.dart
+++ lib/app/modules/withdraw/withdraw_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 class WithdrawView extends StatelessWidget {
   const WithdrawView({super.key});
@@ -6,10 +8,25 @@ class WithdrawView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Center(
-          child: Text('withdraw'),
-        ),
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height*0.25,
+            child: Row(
+              children: [],
+            ),
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+            ),
+          ))
+        ],
       ),
     );
   }
