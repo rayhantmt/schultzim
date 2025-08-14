7bb73ef2d8a69d81ebd4e79b2da0e5b2b9eadb91
rayhantmt <rayhantmt@gmail.com>
2025-08-15 02:17:45 +0600

    added the appbar of withdraw

--- lib/app/modules/withdraw/withdraw_view.dart
+++ lib/app/modules/withdraw/withdraw_view.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:schultzim/app/utils/app_colors.dart';
@@ -12,18 +14,46 @@ class WithdrawView extends StatelessWidget {
       body: Column(
         children: [
           Container(
-            height: Get.height*0.25,
+            height: Get.height*0.20,
             child: Row(
-              children: [],
+              children: [
+                Icon(Icons.arrow_circle_left_outlined,
+                size: 45,
+                color: Colors.white,
+                ),
+                SizedBox(width: Get.width*0.21,),
+                Text('Deposit Successful',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.white
+                ),
+                )
+              ],
             ),
           ),
           Expanded(child: Container(
+            width: double.infinity,
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.only(
                   topLeft: Radius.circular(20),
                   topRight: Radius.circular(20),
                 ),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: 30,),
+                  Text('Current Balance',
+                  style: TextStyle(
+fontWeight: FontWeight.w400,
+fontSize: 14,
+color: Color(0xff050505)
+                  ),
+                  )
+                ],
+              ),
             ),
           ))
         ],
