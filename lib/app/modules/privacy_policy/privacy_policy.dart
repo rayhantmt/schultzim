969b271d30001f7faaa2a2df8eba107dbd8e2689
rayhantmt <rayhantmt@gmail.com>
2025-08-16 00:27:33 +0600

    done with privacy policy

--- lib/app/modules/privacy_policy/privacy_policy.dart
+++ lib/app/modules/privacy_policy/privacy_policy.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+
 
 class PrivacyPolicy extends StatelessWidget {
   const PrivacyPolicy({super.key});
@@ -6,7 +8,29 @@ class PrivacyPolicy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Privacy Policy'),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Privacy Policy',
+        
+        style: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 26,
+          color: Color(0xff3C3C3C)
+        ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SingleChildScrollView(
+          child: Text('Lorem ipsum dolor sit amet consectetur. Mauris cursus laoreet ut egestas. Faucibus elit nunc luctus felis. Nullam faucibus quisque tristique elementum sagittis tempus at lectus. Mattis ipsum amet eu aliquam lorem mi scelerisque sodales viverra. Convallis pretium consequat risus ut augue. Parturient at aliquam egestas diam sed. Sagittis et malesuada sem cras. Maecenas eu aliquam id feugiat gravida nisl ultricies. Integer dui odio nibh quis. Faucibus a luctus amet enim lobortis odio. In turpis sed quam sed etiam senectus bibendum in a. Sagittis dui fringilla morbi sit interdum netus. Lacinia nulla at fusce pellentesque elementum suscipit venenatis. Nisl id aliquet quis id sed cursus vel senectus risus. Neque donec aliquet urna dictumst. Tortor semper lorem lacus ipsum commodo. Eget amet urna montes libero enim in semper. Nisi vivamus consectetur euismod ut eget sit ultricies cursus lectus. Nisl cursus fermentum tempor at.',
+          style: TextStyle(
+            fontWeight: FontWeight.w400,
+            fontSize: 18,
+            color: Colors.black
+          ),
+          ),
+        ),
+      ),
     );
   }
 }
