2c61088de049e4ecdb0595c9540a8bd27a21a403
rayhantmt <rayhantmt@gmail.com>
2025-08-16 00:12:33 +0600

    created the 3 new pagesd

---
+++ lib/app/modules/faq/faq.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Faq extends StatelessWidget {
+  const Faq({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('faq'),
+    );
+  }
+}
