2f84d625ffffca16cbe560df5c812e1ebda88e0f
rayhantmt <rayhantmt@gmail.com>
2025-08-16 03:32:45 +0600

    did some changes and modifications

--- lib/app/modules/hep/help_view.dart
+++ lib/app/modules/hep/help_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
+import 'package:schultzim/app/utils/app_images.dart';
 
 class HelpView extends StatelessWidget {
   const HelpView({super.key});
@@ -17,6 +20,34 @@ class HelpView extends StatelessWidget {
           color: Color(0xff3C3C3C)
         ),
         ),
+      ),
+      body: Column(
+        children: [
+          Image.asset(AppImages.splashlogo,
+          color: AppColors.primarycolor,
+          ),
+          SizedBox(height: 30,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.phone,
+              color: AppColors.primarycolor,
+              ),
+              Text('+354 374747848')
+            ],
+          ),
+          SizedBox(height: 20,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.email,
+              color: AppColors.primarycolor,
+              ),
+              Text('admin@gmail.com')
+            ],
+          )
+          
+        ],
       ),
     );
   }
