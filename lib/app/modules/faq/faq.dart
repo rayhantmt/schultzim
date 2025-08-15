bea1f0c29a592b2dfd03a6175f605a9390a5a0c6
rayhantmt <rayhantmt@gmail.com>
2025-08-16 00:36:46 +0600

    90% of the ui done

--- lib/app/modules/faq/faq.dart
+++ lib/app/modules/faq/faq.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:schultzim/app/utils/app_colors.dart';
 
 class Faq extends StatelessWidget {
   const Faq({super.key});
@@ -6,7 +8,42 @@ class Faq extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('faq'),
+      backgroundColor: AppColors.primarycolor,
+      body: Column(
+        children: [
+          Container(
+            height: Get.height*0.25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => Get.back(),
+                  child: Icon(Icons.arrow_circle_left_outlined,
+                  size: 45,
+                  color: Colors.white,
+                  ),
+                ),
+                Text('Faq',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  fontSize: 18,
+                  color: Colors.white
+                ),
+                )
+              ],
+            ),
+          ),
+          Expanded(child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+            ),
+          ))
+        ],
+      ),
     );
   }
 }
