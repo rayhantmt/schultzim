d9c82c59e783040b9d4347e7ebbc58016d6f33c1
rayhantmt <rayhantmt@gmail.com>
2025-08-20 23:56:22 +0600

    done all the changes

--- lib/app/modules/term_of_use/term_of_use.dart
+++ lib/app/modules/term_of_use/term_of_use.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 
 class TermOfUse extends StatelessWidget {
   const TermOfUse({super.key});
@@ -12,24 +10,38 @@ class TermOfUse extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.white,
         centerTitle: true,
-        title: Text('Terms and Condition',
-        
-        style: TextStyle(
-          fontWeight: FontWeight.w700,
-          fontSize: 26,
-          color: Color(0xff3C3C3C)
-        ),
+        title: Text(
+          'Terms and Condition',
+
+          style: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 26,
+            color: Color(0xff3C3C3C),
+          ),
         ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: SingleChildScrollView(
-          child: Text('Lorem ipsum dolor sit amet consectetur. Mauris cursus laoreet ut egestas. Faucibus elit nunc luctus felis. Nullam faucibus quisque tristique elementum sagittis tempus at lectus. Mattis ipsum amet eu aliquam lorem mi scelerisque sodales viverra. Convallis pretium consequat risus ut augue. Parturient at aliquam egestas diam sed. Sagittis et malesuada sem cras. Maecenas eu aliquam id feugiat gravida nisl ultricies. Integer dui odio nibh quis. Faucibus a luctus amet enim lobortis odio. In turpis sed quam sed etiam senectus bibendum in a. Sagittis dui fringilla morbi sit interdum netus. Lacinia nulla at fusce pellentesque elementum suscipit venenatis. Nisl id aliquet quis id sed cursus vel senectus risus. Neque donec aliquet urna dictumst. Tortor semper lorem lacus ipsum commodo. Eget amet urna montes libero enim in semper. Nisi vivamus consectetur euismod ut eget sit ultricies cursus lectus. Nisl cursus fermentum tempor at.',
-          style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 18,
-            color: Colors.black
-          ),
+          child: Text(
+            """FTAX is not an Advisor. FTAX is merely a tool that helps you plan for taxes.  
+
+Before investing in any mutual fund or exchange-traded fund, you should consider its investment objectives, risks, charges, and expenses. Contact for a prospectus, an offering circular, or, if available, a summary prospectus containing this information. Read it carefully.  
+
+Performance data shown represents past performance and is no guarantee of future results. Investment return and principal value will fluctuate, so you may have a gain or loss when shares are sold. Current performance may be higher or lower than that quoted.  
+
+Source: Fidelity Investments as of.  
+
+You could lose money by investing in a money market fund. Although the fund seeks to preserve the value of your investment at \$1.00 per share, it cannot guarantee it will do so.  
+
+An investment in the fund is not a bank account and is not insured or guaranteed by the Federal Deposit Insurance Corporation or any other government agency.  
+
+FTAX is not a Financial Advisor and is not required to reimburse the fund for losses, and you should not expect that FTAX will provide financial support to the fund at any time, including during periods of market stress.""",
+            style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontSize: 18,
+              color: Colors.black,
+            ),
           ),
         ),
       ),
